cab7567470217cbbf2e5c0b7ef3fcdde142ba6a3
Alex <popaalexovidiu@gmail.com>
2020-10-22 11:39:13 +0300

    CP-4: added dummy impl of get method to local db

--- backend/src/main/java/ro/ubb/Main.java
+++ backend/src/main/java/ro/ubb/Main.java
@@ -1,24 +1,16 @@
 package ro.ubb;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @Slf4j
-public class Main implements CommandLineRunner {
-
-  @Autowired private Environment environment;
+public class Main {
 
   public static void main(String[] args) {
     SpringApplication.run(Main.class, args);
   }
 
-  @Override
-  public void run(String... args) {
-    log.info("Current profile = {}", environment.getActiveProfiles()[0]);
-  }
+
 }
---
+++ backend/src/main/java/ro/ubb/service/AdminService.java
@@ -0,0 +1,11 @@
+package ro.ubb.service;
+
+import ro.ubb.model.Admin;
+
+import java.util.List;
+
+public interface AdminService {
+
+    List<Admin> getAll();
+
+}
