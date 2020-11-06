39c8c2dc8651996fed5443e5b9cfe9c89dd8c6ec
Gloria0105 <raie2612@scs.ubbcluj.ro>
2020-11-06 13:39:54 +0200

    added swagger

--- backend/src/main/java/ro/ubb/Main.java
+++ backend/src/main/java/ro/ubb/Main.java
@@ -3,9 +3,13 @@ package ro.ubb;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @Slf4j
+@EnableSwagger2
 public class Main  {
 
   public static void main(String[] args) {
