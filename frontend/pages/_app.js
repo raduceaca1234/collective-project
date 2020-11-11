1bd933a10e1fdfdb18bf2116e619843c69255355
Andreea Corlaci <caie2507@scs.ubbcluj.ro>
2020-11-11 20:30:01 +0200

    CTA and footer

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,12 +2,18 @@ import '../styles/global.scss'
 
 import Navbar from '../components/navbar'
 import Header from '../components/header'
+import Footer from '../components/footer'
+import Cta from '../components/announcements/cta'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Navbar />
       <Component {...pageProps} />
+      <Header></Header>
+      <Cta></Cta>
+      <Footer></Footer>
+      
     </div>
   )
 
