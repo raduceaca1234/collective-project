7322280c87e81d53ca3a95574a9655d626296bed
raduceaca1234 <ceaca.radu@yahoo.com>
2020-11-14 17:44:35 +0200

    habar n-am

---
+++ frontend/pages/add.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import AddProduct from '../components/add_product';
+
+const Add = () => {
+    return (
+        <div>
+            <AddProduct/>
+        </div>
+    )
+}
+
+export default Add;
