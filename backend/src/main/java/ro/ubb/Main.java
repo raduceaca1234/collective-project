0f1a11a75fa33c39326c1ba09ed3b2a0f73d680f
Gloria0105 <raie2612@scs.ubbcluj.ro>
2021-03-21 21:24:38 +0200

    connecting to the database doesn't work

--- backend/src/main/java/ro/ubb/Main.java
+++ backend/src/main/java/ro/ubb/Main.java
@@ -3,8 +3,6 @@ package ro.ubb;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
